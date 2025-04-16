2e4a907b960c83155ced4dcb41761a5822e7e9fb
ctschnurr <ctschnurr@hotmail.com>
2025-04-16 11:09:20 -0300

    Attempting another fix for build error

--- app/(dashboard)/myuseraccount/page.tsx
+++ app/(dashboard)/myuseraccount/page.tsx
@@ -108,6 +108,10 @@ export default function GeneralPage() {
     });
   };
 
+  if (!user) {
+    return <div>Loading...</div>; // Or a fallback message/loading spinner
+  }
+
   return (
     <section className="flex-1 p-4 lg:p-8">
       <h1 className="text-lg lg:text-2xl font-medium text-gray-900 mb-6">
